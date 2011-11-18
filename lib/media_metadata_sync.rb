a8baf9e48523105e42a23044590c732108aa942a
Andy Hartford <hartforda@gmail.com>
2011-11-17 16:17:01 -0800

    Get the itunes applescript stuff working again.

--- lib/media_metadata_sync.rb
+++ lib/media_metadata_sync.rb
@@ -1,10 +1,11 @@
 require 'active_support'
+require 'pathname'
 
 lib = Pathname.new(__FILE__).dirname
 require lib.join("media_metadata_sync/version")
 #require lib.join('media_metadata_sync/script/itunes_to_musicbrainz')
 require lib.join('media_metadata_sync/source/file_system')
-#require lib.join('media_metadata_sync/source/itunes')
+require lib.join('media_metadata_sync/source/itunes')
 #require lib.join('media_metadata_sync/source/mb')
 
 module MediaMetadataSync
--- lib/media_metadata_sync/source/itunes.rb
+++ lib/media_metadata_sync/source/itunes.rb
@@ -1,31 +1,33 @@
 require 'appscript'
 
-module Source
-  module ITunes
+module MediaMetadataSync
+  module Source
+    module ITunes
 
-    # Return a hash of track ratings with the following
-    # structure: {Pathname => {'album_rating' => 4, 'rating' =>
-    # 1}}
-    def self.track_ratings
-      app = Appscript.app('iTunes')
+      # Return a hash of track ratings with the following
+      # structure: {Pathname => {'album_rating' => 4, 'rating' =>
+      # 1}}
+      def self.track_ratings
+        app = Appscript.app('iTunes')
 
-      info = {}
+        info = {}
 
-      # this assumes the order of track remains the same
-      # between applescript calls
-      %w(album_rating rating location).each do |attr|
-        app.file_tracks.send(attr).get.each_with_index do |val, i|
-          (info[i] ||= {})[attr.to_sym] = val
+        # this assumes the order of track remains the same
+        # between applescript calls
+        %w(album_rating rating location).each do |attr|
+          app.file_tracks.send(attr).get.each_with_index do |val, i|
+            (info[i] ||= {})[attr.to_sym] = val
+          end
         end
-      end
 
-      info.values.inject({}) do |memo, info| 
-        memo[Pathname.new info.delete(:location).path] = {
-          :rating => info[:rating] / 20.0,
-          :album_rating => info[:album_rating] / 20.0,
-        }
+        info.values.inject({}) do |memo, info| 
+          memo[Pathname.new info.delete(:location).path] = {
+            :rating => info[:rating] / 20.0,
+            :album_rating => info[:album_rating] / 20.0,
+          }
 
-        memo
+          memo
+        end
       end
     end
   end
